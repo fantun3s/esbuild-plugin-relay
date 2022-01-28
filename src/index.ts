b297a7605e4f74e88036f21d603f93e94202e020
Fabio Antunes <fabio.antunes@thetrainline.com>
2022-01-28 17:52:55 +0000

    Use correct loader for file extension

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,34 @@
-import type { Plugin } from "esbuild";
+import type { Loader, Plugin } from "esbuild";
 import * as fs from "fs";
 import { compile } from "./compile";
 import { PluginOptions, setup } from "./setup";
 
-export default function createRelayPlugin(opts?: PluginOptions): Plugin {
-  return {
+const extensionToLoader = {
+  ts: 'ts',
+  mts: 'ts',
+  tsx: 'tsx',
+  js: 'js',
+  mjs: 'js',
+  jsx: 'jsx',
+}
+export const relayPlugin = (opts?: PluginOptions): Plugin => ({
     name: "relay",
     setup(build) {
       const { options, compileOptions } = setup(build, opts);
 
-      build.onLoad({ filter: options.filter }, async (args) => {
+      build.onLoad({ filter: options.filter}, async (args) => {
         let contents = await fs.promises.readFile(args.path, "utf8");
+        const fileExtension = args.path.includes('.') && args.path.split('.').pop();
         if (contents.includes("graphql`")) {
           contents = compile(args.path, contents, compileOptions);
         }
 
         return {
           contents: contents,
-          loader: options.loader || "jsx",
+          loader: options.loader || extensionToLoader[fileExtension] as Loader || 'jsx',
         };
       });
     },
-  };
-}
+})
+
+export default relayPlugin
